6eea73a8022b7df020705d95c32c2eebeb72accd
SiMun Jang <seamoon.1029@gmail.com>
2023-12-15 17:53:47 +0900

    Q2557
    * 문제 URL : https://www.acmicpc.net/problem/2557
    * 문제 제목 : Hello World
    * 문제 내용
    * Hello World!를 출력하시오.

---
+++ src/baekjoon/Q2557.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+/**
+ * 문제 URL : https://www.acmicpc.net/problem/2557
+ * 문제 제목 : Hello World
+ * 문제 내용
+ * Hello World!를 출력하시오.
+ */
+public class Q2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
